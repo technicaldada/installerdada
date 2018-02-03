215455c903fbf56e06f1cd581287e62b451e4ebf
Technical Dada <32122182+technicaldada@users.noreply.github.com>
2018-02-03 12:43:43 +0530

    Add files via upload

---
+++ installerdada.sh
@@ -0,0 +1,10 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+clear
+
+apt update
+apt install python2
+apt install git
+git clone https://github.com/technicaldada/installerdada
+cd installerdada
+pyrhon2 installerdada.py
